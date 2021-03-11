19615764cce23e4df821bef786da7a872566aa73
Adam Earle <robotlightsyou@gmail.com>
2021-03-11 00:27:35 -0800

    json working
    added a demo file to test some functionality attempting to load a plot from a json file and navigate from option to option until you reach an endpoint. Currently fully functional in demo file, but I had to copy init.py into utils.py because I didn't know how to import from init without working in main and running the module. This code isn't ready for main yet, hence the workaround.

--- adventure_colussus/sample_game.py
+++ adventure_colussus/sample_game.py
@@ -4,42 +4,40 @@ import utils as ac
 
 
 def main():
-    with open("../plots_json/all_quests.json") as jsonfile:
+    with open("./plots_json/all_quests.json") as jsonfile:
         quests = json.load(jsonfile)
 
-    # ac.print_text("What quest would you like to endeavor today?" + "\n")
-    print("What quest would you like to endeavor today?" + "\n")
+    ac.print_text("What quest would you like to endeavor today?" + "\n")
+    # print("What quest would you like to endeavor today?" + "\n")
     filenames = []
     for index, option in enumerate(quests):
-        # ac.print_text("\t" + str(index + 1) + ") " + quests[quest]["name"] + " | " + quests[quest]["description"] + "\n")
-        print("\t" + str(index + 1) + ") " +
-              quests[option]["name"] + " | " + quests[option]["description"] + "\n")
+        ac.print_text("\t" + str(index + 1) + ") " +
+                      quests[option]["name"] + " | " + quests[option]["description"] + "\n")
+        # print("\t" + str(index + 1) + ") " +
+        #   quests[option]["name"] + " | " + quests[option]["description"] + "\n")
         filenames.append(quests[option]["filename"])
     responses = [str(i) for i in range(1, len(quests) + 1)]
     answer = ac.get_input(">", responses)
     quest_file = filenames[int(answer)-1]
-    # quest_file = "../plots_json/" + quest_file
-    # print(quest_file + "\n" + str(type(quest_file)))
     with open(quest_file) as qf:
         quest = json.load(qf)
-    userchoice = print_question(quest["one"], quest)
-    # while userchoice != "Null":
-    #     print_question(quest[userchoice], quest)
+    print_question(quest["one"], quest)
 
 
 def print_question(question, quest):
-    print(question["ask"])
+    # print(question["ask"])
+    ac.print_text(question["ask"] + "\n")
     for index, option in enumerate(question["options"]):
-        print(f"{index}: {option}")
-    test = input(">")
-    user_choice = [int(test)]
-    # user_choice = quest[question]["options"][int(test)]
-    # user_choice = quest[question]["next"][(quest[question]["options"][int(test)])]
-    print(f"{quest[question]}")
-    # while test != "Null":
-    #     return print_question(quest[test], quest)
-    # return "You have reached the end of your quest."
-    # return "twoa"
+        ac.print_text(f"{index + 1}: {option}\n")
+    responses = [str(i) for i in range(1, len(question["options"]) + 1)]
+    # answer = input(">")
+    answer = ac.get_input(">", responses)
+    user_choice = question["next"][(question["options"][int(answer) - 1])]
+    while user_choice != "Null":
+        return print_question(quest[user_choice], quest)
+    # print("You have reached the end of your quest.")
+    ac.print_text("You have reached the end of your quest.\n")
+    return 0
 
 
 if __name__ == "__main__":
