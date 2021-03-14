eb7bdccd29475154054634b4e293933a5c74f94e
robotlightsyou <robotlightsyou@gmail.com>
2021-03-13 19:08:11 -0800

    adding json to main

--- adventure_colussus/__init__.py
+++ adventure_colussus/__init__.py
@@ -54,6 +54,14 @@ def print_text(text: str, sleep_time: float = 0.0) -> None:
         time.sleep(sleep_time)
 
 
+def print_block(lines: dict) -> None:
+    """ takes dictionary where
+    keys: string to print
+    values: float wait time"""
+    for key in lines.keys():
+        print_text(key, lines[key])
+
+
 def save_character(save_name: str, character: Dict[str, Any]) -> None:
     """
     Saves the current character to a pickle database.
@@ -65,18 +73,18 @@ def save_character(save_name: str, character: Dict[str, Any]) -> None:
         print_text(' > Character saved successfully')
 
 
-def load_character(load_name):
-    """
-    Loads the selected character from a pickle database.
-    """
-    load_name_pickle = load_name + '.pickle'
-    print_text(' > Loading character...\n', 1)
-    pickle_in = open(load_name_pickle, "rb")
-    character = pickle.load(pickle_in)
-    print_text(' > Character loaded successfully\n')
-    print_text(f"\n > Welcome back {character['name']}!!!\n", 0.5)
-    print_text('\n > Here are your stats from last time: \n', 0.5)
-    print(f' > {character} ')
+# def load_character(load_name):
+#     """
+#     Loads the selected character from a pickle database.
+#     """
+#     load_name_pickle = load_name + '.pickle'
+#     print_text(' > Loading character...\n', 1)
+#     pickle_in = open(load_name_pickle, "rb")
+#     character = pickle.load(pickle_in)
+#     print_text(' > Character loaded successfully\n')
+#     print_text(f"\n > Welcome back {character['name']}!!!\n", 0.5)
+#     print_text('\n > Here are your stats from last time: \n', 0.5)
+#     print(f' > {character} ')
 
 
 def character_generator():
@@ -84,28 +92,31 @@ def character_generator():
     Generates a character determined by user input and saves it in
     a pickle database.
     """
-    print_text('\n > We have reached the first crucial part of your journey: We must choose the path that you will take! The decision\n', 0.5)
-    print_text(' > is up to you my friend! Whether you choose to be a bloodthirsty warrior or a cunning and strategic fighter,\n', 0.8)
-    print_text(' > the choice is up to you!\n', 0.5)
-    print_text('\n > Now then, lets get right into it!', 0.8)
-    print_text(' Are you more of a tanky player[1] or a strategic player[2]?')
-
+    character_style_menu = {
+        "\n > We have reached the first crucial part of your journey: We must choose the path that you will take! The decision\n": 0.5,
+        " > is up to you my friend! Whether you choose to be a bloodthirsty warrior or a cunning and strategic fighter,\n" : 0.8,
+        " > the choice is up to you!\n": 0.5,
+        "\n > Now then, lets get right into it!": 0.8,
+        " Are you more of a tanky player[1] or a strategic player[2]?": 0.0
+    }
+    print_block(character_style_menu)
     player_t_choice_1 = get_input('\n > ', ['1', '2'])
 
     if player_t_choice_1 == '1':
-        health = 100
+        total_health = 100
         damage = 75
 
     elif player_t_choice_1 == '2':
-        health = 75
+        total_health = 75
         damage = 50
 
-    print_text('\n > So, we have that out of the way, Let\'s carry on!', 0.8)
-    print_text(' Oh of course! Sorry to forget! Another quick question: ', 0.5)
-    print_text('Do you like \n')
-    print_text(
-        ' > to use magic from great distances[1] or run in to the thick of battle wielding a deadly blade[2]? ')
-
+    attack_style_menu = {
+        "\n > So, we have that out of the way, Let's carry on! ": 0.8,
+        "Oh of course! Sorry to forget! Another quick question: ": 0.5,
+        "Do you like \n": 0.5,
+        " > to use magic from great distances[1] or run in to the thick of battle wielding a deadly blade[2]?\n": 0.5,
+    }
+    print_block(attack_style_menu)
     player_t_choice_2 = get_input('\n > ', ['1', '2'])
 
     if player_t_choice_2 == '1':
@@ -116,56 +127,62 @@ def character_generator():
         shield = 75
         magic = 45
 
-    print_text(
-        '\n > Good good! We have decided your play style and your preferred ways of attacking the enemy!\n', 0.5)
-    print_text(
-        ' > Now, we must see what luck we are able to bestow upon you. Be warned: it is entirely random!\n', 0.8)
-    random_luck = input('\n > Press enter to roll a dice...')
+    luck_menu = {
+        "\n > Good good! We have decided your play style and your preferred ways of attacking the enemy!\n": 0.5,
+        " > Now, we must see what luck we are able to bestow upon you. Be warned: it is entirely random!\n": 0.8
+    }
+    print_block(luck_menu)
+    input('\n > Press enter to roll a dice...')
     time.sleep(0.3)
     print_text(' > Rolling dice...\n')
     luck = random.randint(0, 10)
     time.sleep(1)
     print_text(f' > Your hero has {luck} luck out of 10!\n', 0.8)
-    print_text(
-        '\n > At last! We have reached the most important part of creating your character! The naming!\n')
-    print_text(
-        ' > Choose wisely my friend. Your hero will be named this for the rest of their lives...\n', 1)
-
-    print_text('\n > What should your hero be named?\n ')
+    
+    name_menu = {
+        "\n > At last! We have reached the most important part of creating your character! The naming!\n": 0.0,
+        " > Choose wisely my friend. Your hero will be named this for the rest of their lives...\n": 1,
+        "\n > What should your hero be named?\n": 0.0
+    }
+    print_block(name_menu)
 
     name = ''
     while not name:
         name = input(' > ')
 
     time.sleep(1)
-    print_text(
-        f'\n > Welcome mighty hero! You shall henceforth be known as: {name} !!!\n ', 0.3)
-    print_text('> A perfect choice!')
-    print_text('\n')
 
-    print_text(
-        ' \n > Now then. I guess you should be on your way! You have a journey to start and a belly to fill!\n')
+    menu_wrap = {
+        f"\n > Welcome mighty hero! You shall henceforth be known as: {name} !!!\n ": 0.3,
+        "> A perfect choice!\n": 0.0,
+        " \n > Now then. I guess you should be on your way! You have a journey to start and a belly to fill!\n": 0.0
+    }
+    print_block(menu_wrap)
+
+    parting_text = {
+        " > I have to say, I have rather enjoyed your company! Feel free to come by at any time!\n": 0.0,
+        " > Goodbye and god speed!\n": 1,
+        " > Your starting stats are as follows:\n": 0.3,
+        f" > [ health: {total_health}, damage: {damage}, shield: {shield}, magic: {magic}, luck: {luck}, name: {name} ]": 0.0,
+        "\n > We should now save your character if you want to come back to it later - character file name:\n": 0.0
+    }
+    print_block(parting_text)
 
     # Should Implement The Script And Story From Here.
 
-    print_text(
-        ' > I have to say, I have rather enjoyed your company! Feel free to come by at any time!\n ')
-    print_text('> Goodbye and god speed!', 1)
-    print('\n')
-    print_text(' > Your final stats are as follows: \n', 0.3)
-    print(
-        f" > [ health: {health}, damage: {damage}, shield: {shield}, magic: {magic}, luck: {luck}, name: {name} ]")
-
-    print_text(
-        '\n > We should now save your character if you want to come back to it later - character file name: \n ')
-    character_dict = {'health': health, 'damage': damage,
+    character_dict = {'health': total_health, 'damage': damage,
                       'shield': shield, 'magic': magic, 'luck': luck, 'name': name}
 
     # Generate an initial character
     character = entities.Human.generate(name)
 
     # Update the character
-    character = entities.Human(**(dict(character) | character_dict))
+    if player_t_choice_1 == "1":
+        character = entities.Brawler(**(dict(character) | character_dict))
+    elif player_t_choice_1 == "2":
+        character = entities.Ranger(**(dict(character) | character_dict))
+    else:
+        character = entities.Brawler(**(dict(character) | character_dict))
 
     # Ask for the file name
     character_file_name = input('> ')
@@ -213,78 +230,76 @@ I don't know what this is but it's causing a syntax error.
 == == == =
 """
 
-def main_menu():
+def main_menu(CLEAR_SCREEN='clear'):
     """
     This is where everything to do with the main game is. This includes all functions in one
     way or another. 
     """
+    
     time.sleep(0.5)
     system(CLEAR_SCREEN)
     time.sleep(0.5)
-    '''
-    change datetime format no seconds
-    '''
     show_date_and_time = datetime.datetime.now()
     screen_line()
-    print('\n  <Adventure Colossus>         session: ', session_count,
+    print('\n  <Adventure Colossus>         session:', session_count,
           '| current date: ', show_date_and_time, '| date of creation: 9.2.2021')
     screen_line()
     time.sleep(0.5)
     mountain_range()
     screen_line()
-    '''
-    encapsulate in print_block() call
-    '''
-    print('\n > [1] Create new game')
-    print(' > [2] Load existing game')
-    print(' > [3] End game')
-    print(' > [4] Credits')
-    '''
-    add choices dictionary. keys are answers, values are next_steps
-    choices = {1: "charachter_generator", 2: "load_character"}
-    choice = get_input("\n > " [str(key) for key in choices.keys()])
-    choices[choice]()
-    '''
+    create_load_menu = {
+        "> [1] Create new game\n": 0.5,
+        "> [2] Load existing game\n": 0.5,
+        "> [3] End game\n": 0.5,
+        "> [4] Credits\n": 0.5
+    }
+    print_block(create_load_menu)
     choice = get_input("\n > ", ['1', '2', '3', '4'])
+    return choice
 
-    if choice == '1':
-        print_text(
-            "\n > You have chosen to create a new game: Redirecting...", 0.75)
-        system(CLEAR_SCREEN)
-        screen_line()
-        print('  \n  We will begin with creating your character:                                        Quick tip: Choose wisely')
-        screen_line()
-        time.sleep(0.5)
-        character_selection_horse_and_knight()
-        screen_line()
-        time.sleep(0.3)
-        character_generator()
+def new_character(CLEAR_SCREEN='clear'):
+    print_text(
+        "\n > You have chosen to create a new game: Redirecting...", 0.75)
+    system(CLEAR_SCREEN)
+    screen_line()
+    print('  \n  We will begin with creating your character:                                        Quick tip: Choose wisely')
+    screen_line()
+    time.sleep(0.5)
+    character_selection_horse_and_knight()
+    screen_line()
+    time.sleep(0.3)
+    return character_generator()
 
-    elif choice == '2':
-        print_text(
-            "\n > You have chosen to load an existing game: Redirecting...", 0.75)
-        system(CLEAR_SCREEN)
-        time.sleep(0.5)
-        screen_line()
-        print('  \n  We will begin with choosing an existing character:                             Quick tip: Make sure it exists!')
-        screen_line()
-        time.sleep(0.5)
-        character_selection_horse_and_knight()
-        screen_line()
-        character_file_name = input('\n > Character file name: ')
-        load_character(character_file_name)
+    # elif choice == '2':
+def load_character(CLEAR_SCREEN='clear'):
+    print_text(
+        "\n > You have chosen to load an existing game: Redirecting...", 0.75)
+    system(CLEAR_SCREEN)
+    time.sleep(0.5)
+    screen_line()
+    print('  \n  We will begin with choosing an existing character:                             Quick tip: Make sure it exists!')
+    screen_line()
+    time.sleep(0.5)
+    character_selection_horse_and_knight()
+    screen_line()
+    character_file_name = input('\n > Character file name: ')
+    # The new character load
+    character = entities.Human.load(f"./{character_file_name}.dat")
+    print(character.__repr__())
+    return character
 
-    elif choice == '3':
-        print_text(' > Ending session...', 0.5)
-        print_text(' > Session ended successfully \n', 1)
-        sys.exit()
+    # elif choice == '3':
+    #     print_text(' > Ending session...', 0.5)
+    #     print_text(' > Session ended successfully \n', 1)
+    #     sys.exit()
 
-    elif choice == '4':
-        pass
+    # elif choice == '4':
+    #     pass
 
-    else:
-        print_text('Invalid response. Please try again')
+    # else:
+    #     print_text('Invalid response. Please try again')
 
 
 if __name__ == '__main__':
-    main()
+    pass
+    
--- adventure_colussus/__main__.py
+++ adventure_colussus/__main__.py
@@ -1,6 +1,7 @@
 from adventure_colussus import *
 import time
 import random
+import adventure_colussus.json_handlers as jh
 
 
 CLEAR_SCREEN = 'clear'
@@ -12,82 +13,71 @@ def main():
     This is where everything to do with the main game is. This includes all functions in one
     way or another. 
     """
-    time.sleep(0.5)
-    system(CLEAR_SCREEN)
-    time.sleep(0.5)
-    show_date_and_time = datetime.datetime.now()
-    screen_line()
-    print('\n  <Adventure Colossus>         session:', session_count,
-          '| current date: ', show_date_and_time, '| date of creation: 9.2.2021')
-    screen_line()
-    time.sleep(0.5)
-    mountain_range()
-    screen_line()
-    create_load_menu = {
-        "> [1] Create new game\n": 0.5,
-        "> [2] Load existing game\n": 0.5,
-        "> [3] End game\n": 0.5,
-        "> [4] Credits\n": 0.5
-    }
-    print_block(create_load_menu)
-    choice = get_input("\n > ", ['1', '2', '3', '4'])
+    choice = main_menu(CLEAR_SCREEN)
 
     if choice == '1':
-        print_text(
-            "\n > You have chosen to create a new game: Redirecting...", 0.75)
-        system(CLEAR_SCREEN)
-        screen_line()
-        print('  \n  We will begin with creating your character:                                        Quick tip: Choose wisely')
-        screen_line()
-        time.sleep(0.5)
-        character_selection_horse_and_knight()
-        screen_line()
-        time.sleep(0.3)
-        character_generator()
-
+        new_character(CLEAR_SCREEN)
     elif choice == '2':
-        print_text(
-            "\n > You have chosen to load an existing game: Redirecting...", 0.75)
-        system(CLEAR_SCREEN)
-        time.sleep(0.5)
-        screen_line()
-        print('  \n  We will begin with choosing an existing character:                             Quick tip: Make sure it exists!')
-        screen_line()
-        time.sleep(0.5)
-        character_selection_horse_and_knight()
-        screen_line()
-        character_file_name = input('\n > Character file name: ')
-        #load_character(character_file_name)
-
-        # The new character load
-        character = entities.Human.load(f"./{character_file_name}.dat")
-        # tests
-        bob = entities.Ranger.generate('Bob')
-        jim = entities.Brawler.generate('Jim')
-        print(jim.attack(1, bob))
-        print(bob.attack(1, jim))
-        print(jim.attack(2, bob))
-        print(bob.attack(1, jim))
-        print(jim.attack(1, bob))
-        print(bob.attack(2, jim))
-
-        z = entities.Zombie.generate()
-
-        while z.current_health > 0:
-            print(jim.attack(1, z))
-            print(bob.attack(2, z))
-
+        load_character(CLEAR_SCREEN)
     elif choice == '3':
         print_text(' > Ending session...', 0.5)
         print_text(' > Session ended successfully \n', 1)
         sys.exit()
-
     elif choice == '4':
-        pass
-
+        print_text("exiting...", 1)
+        sys.exit(0)
     else:
         print_text('Invalid response. Please try again')
 
+
+    quest = jh.load_quest()
+    print(quest)
+
+    # if choice == '1':
+        # print_text(
+        #     "\n > You have chosen to create a new game: Redirecting...", 0.75)
+        # system(CLEAR_SCREEN)
+        # screen_line()
+        # print('  \n  We will begin with creating your character:                                        Quick tip: Choose wisely')
+        # screen_line()
+        # time.sleep(0.5)
+        # character_selection_horse_and_knight()
+        # screen_line()
+        # time.sleep(0.3)
+        # character_generator()
+
+    # elif choice == '2':
+        # print_text(
+        #     "\n > You have chosen to load an existing game: Redirecting...", 0.75)
+        # system(CLEAR_SCREEN)
+        # time.sleep(0.5)
+        # screen_line()
+        # print('  \n  We will begin with choosing an existing character:                             Quick tip: Make sure it exists!')
+        # screen_line()
+        # time.sleep(0.5)
+        # character_selection_horse_and_knight()
+        # screen_line()
+        # character_file_name = input('\n > Character file name: ')
+        # #load_character(character_file_name)
+
+        # # The new character load
+        # character = entities.Human.load(f"./{character_file_name}.dat")
+        # print(character.__repr__())
+        # tests
+        # bob = entities.Ranger.generate('Bob')
+        # jim = entities.Brawler.generate('Jim')
+        # print(jim.attack(1, bob))
+        # print(bob.attack(1, jim))
+        # print(jim.attack(2, bob))
+        # print(bob.attack(1, jim))
+        # print(jim.attack(1, bob))
+        # print(bob.attack(2, jim))
+
+        # z = entities.Zombie.generate()
+
+        # while z.current_health > 0:
+        #     print(jim.attack(1, z))
+        #     print(bob.attack(2, z))
 
 if __name__ == '__main__':
     main()
